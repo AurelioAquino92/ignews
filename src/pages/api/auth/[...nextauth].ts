e92a53c244f01766cc06819e63c43aadde63ae8f
Aurelio Aquino <aurelio.obmep@gmail.com>
2022-11-26 09:34:52 -0400

    add: fauna db create

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -15,6 +17,27 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      
+      } catch {
+      
+        return false
+
+      }
+    }
+  }
 }
 
 export default NextAuth(authOptions)
